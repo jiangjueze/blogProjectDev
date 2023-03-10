c8a11343ef78251bce7866cb5b7046ed794f3f7e
jiangjueze <jiangjueze@eswincomputing.com>
2023-03-10 17:49:27 +0800

    [feat]第6章第2节

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="detail">
-    博客详情
+    <section class="user-info">
+      <img
+        :src="user.avatar"
+        :alt="user.username"
+        :title="user.username"
+        class="avatar"
+      />
+      <h3>{{ title }}</h3>
+      <p>
+        <router-link :to="`/user/${user.id}`">{{ user.username }}</router-link>
+        发布于{{ friendlyDate(createdAt) }}
+      </p>
+    </section>
+    <section class="article" v-html="markdown"></section>
   </div>
 </template>
 
 <script src="./template.js"></script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./template.css"></style>
+<style src="./template.less" lang="less"></style>
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="index">
-    <p>首页</p>
-    <div>
-      <el-button @click="onClick1">默认按钮</el-button>
-      <el-button @click="onClick2">alert</el-button>
-    </div>
+    <section class="blog-posts">
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="" />
+          <figcaption>若愚</figcaption>
+        </figure>
+        <h3>前端异步大揭秘 <span>3天前</span></h3>
+        <p>
+          本文以一个简单的文件读写为例，讲解了异步的不同写法，包括普通的callback、Es2016中的Promise和Generator、
+          Node用于解决回调的co模块、Es2017中的async/await。适合初步接触Node.js以及少量Es6语法的同学阅读...
+        </p>
+      </div>
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="" />
+          <figcaption>若愚</figcaption>
+        </figure>
+        <h3>前端异步大揭秘 <span>3天前</span></h3>
+        <p>
+          本文以一个简单的文件读写为例，讲解了异步的不同写法，包括普通的callback、Es2016中的Promise和Generator、
+          Node用于解决回调的co模块、Es2017中的async/await。适合初步接触Node.js以及少量Es6语法的同学阅读...
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
